139fd19080214e2cf8339589c8e731b903f828b7
チョン <vot750422@gmail.com>
2026-01-15 13:44:45 +0700

    Các kiểu Print của JAVA

---
+++ CacKieuPrint/BaiTap.java
@@ -0,0 +1,15 @@
+package CacKieuPrint;
+
+//-in ra chuỗi "tôi tên là Phan Minh Trọng và tôi năm nay 20 tuổi , hiện tại tôi sống ở Cần Thơ
+public class BaiTap {
+	public static void main(String[] args) {
+		int age = 20;
+		String name = "Phan Minh Trọng";
+		String Country = "Cần Thơ";
+		// cách 1 : nối chuỗi
+		System.out.println(
+				"Tôi tên là " + name + " và tôi năm nay " + age + "" + " tuổi , hiện tại tôi sống ở " + Country);
+		// cách 2 : dùng định dạng format
+		System.out.printf("Tôi tên là %s và tôi năm nay %d , hiện tại tôi sống ở %s", name, age, Country);
+	}
+}
---
+++ CacKieuPrint/CacKieuPrint.java
@@ -0,0 +1,24 @@
+package CacKieuPrint;
+
+public class CacKieuPrint {
+	public static void main(String[] args) {
+		System.out.print("TrongDepTrai");// -chỉ in ra mà
+		// mà không quan tâm có xuống dòng hay không
+		System.out.print("Trọng đẹp trai\n");
+		System.out.println("Trọng đẹp trai");
+		// -in ra và sẽ tự động xuống dòng nội dung tiếp theo
+		System.out.print("Trọng đẹp trai");
+
+		System.out.printf("Trọng nghèo có chỉ có %d VNĐ\n", 50000);
+		System.out.printf("Trọng nghèo có chỉ có %f VNĐ", 50000.59);
+		// -code in ra sẽ có định dạng format
+		// -có 2 loại %d là kiểu số nguyên còn %f là kiểu số thực
+		System.out.printf("Trọng nghèo có chỉ có %.4f VNĐ\n", 50000.9842402);
+		// -%f nếu muốn lấy n số thập phân phía sau thì code là %.nf trong đó n là số số
+		// thập phân
+		// muốn hiện ra
+		System.out.printf("Tôi tên là %s và hiện tại tôi sống ở %s", "Trọng", "Hậu Giang");
+		// -%s cũng giống với %d nhưng chỉ áp dụng cho chuỗi và chuỗi pahri đặt trong
+		// cặp "
+	}
+}
