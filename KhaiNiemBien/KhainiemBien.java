1f7ae7b6e6724e4839e88119a5295bc82bbf09f5
チョン <vot750422@gmail.com>
2026-01-15 13:45:31 +0700

    Biến trong java

---
+++ KhaiNiemBien/KhainiemBien.java
@@ -0,0 +1,11 @@
+package KhaiNiemBien;
+
+public class KhainiemBien {
+	public static void main(String[] args) {
+		int a = 6;
+		int b = 9;
+		int c = a + b;
+		System.out.println("Sum a + b = " + c);
+	}
+
+}
