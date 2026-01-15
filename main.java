0cc89a0b5fadf53178489324bba71ec02bd76539
チョン <vot750422@gmail.com>
2026-01-15 14:36:28 +0700

    Code gửi từ VSCODE

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        concho cho = new concho();
+        cho.LoaiCho = "Phú Quốc";
+        cho.MauLong = "Vàng";
+        cho.Tuoi = 3;
+        cho.sua();
+        cho.an();
+        cho.Chui();
+    }
+
+}
