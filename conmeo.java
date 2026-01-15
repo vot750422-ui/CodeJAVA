0bfeb824e13ee4f0e78a9724a7e7da525e3213dc
チョン <vot750422@gmail.com>
2026-01-15 23:38:16 +0700

    THEM VAO CON MEO THU 2

--- conmeo.java
+++ conmeo.java
@@ -1,15 +1,26 @@
 public class conmeo {
-    public String ten;
-    public int tuoi;
-    public String mauLong;
-    public String giong;
-    public void keu(){
-        System.out.println("Meo Meo");
-    }
-    public void an(){
-        System.out.println("Ngoàm Ngoàm");
-    }
-    public void ngu(){
-        System.out.println("Zzz Zzz");
-    }
+	public String ten;
+	public int tuoi;
+	public String mauLong;
+	public String giong;
+	public String ten1;
+	public String giong1;
+	public String mauLong1;
+	public int tuoi1;
+
+	public void keu() {
+		System.out.println("Meo Meo");
+	}
+
+	public void an() {
+		System.out.println("Ngoàm Ngoàm");
+	}
+
+	public void ngu() {
+		System.out.println("Zzz Zzz");
+	}
+
+	public void ia() {
+		System.out.println("PHẸT PHẸT");
+	}
 }
